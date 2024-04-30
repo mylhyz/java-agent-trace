0d3ac332ca0bb6390344302f0a1714f8d69763fc
mylhyz <mylhyz@gmail.com>
2024-04-30 10:49:00 +0800

    配置logging

--- src/main/java/io/mylhyz/viper/ClassLoggerTransformer.java
+++ src/main/java/io/mylhyz/viper/ClassLoggerTransformer.java
@@ -1,14 +1,19 @@
 package io.mylhyz.viper;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
 
 public class ClassLoggerTransformer implements ClassFileTransformer {
 
+    final Logger logger = LoggerFactory.getLogger(getClass());
+
     @Override
     public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classFileBuffer) throws IllegalClassFormatException {
-        System.out.println("[TRACE]visit " + className);
+        logger.info("visit {}", className);
         return classFileBuffer;
     }
 }
